96353d16862058b3f456f40153d336b8f07b05d1
making-a-scene <4936joo@naver.com>
2024-01-04 17:15:20 +0900

    프로젝트 초기 설정

---
+++ src/main/java/com/smwu/dagawa/DagawaApplication.java
@@ -0,0 +1,13 @@
+package com.smwu.dagawa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DagawaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DagawaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smwu/dagawa/DagawaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smwu.dagawa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DagawaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
